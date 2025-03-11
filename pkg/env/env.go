5af2fbbe86f38e971b5bb6eb0d5c01049c6d989b
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-11 18:11:16 +0700

    feat(env): add environment management with dev and pro modes

---
+++ pkg/env/env.go
@@ -0,0 +1,61 @@
+package env
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+const (
+	// Dev is development environment mode
+	Dev string = "dev"
+
+	// Pro is production environment mode
+	Pro string = "pro"
+)
+
+var (
+	_      Environment = (*environment)(nil)
+	active Environment
+	devEnv Environment = &environment{value: Dev}
+	proEnv Environment = &environment{value: Pro}
+)
+
+type (
+	Environment interface {
+		Value() string
+		IsDev() bool
+		IsPro() bool
+	}
+
+	environment struct{ value string }
+)
+
+func Active() Environment {
+	return active
+}
+
+func (e *environment) Value() string {
+	return e.value
+}
+
+func (e *environment) IsDev() bool {
+	return e.value == Dev
+}
+
+func (e *environment) IsPro() bool {
+	return e.value == Pro
+}
+
+func init() {
+	env := flag.String("env", "", "Please enter the environment (dev or pro):")
+	flag.Parse()
+
+	switch strings.ToLower(strings.TrimSpace(*env)) {
+	case Pro:
+		active = proEnv
+	default:
+		active = devEnv
+		fmt.Println("Warning: '-env' can not be found or is it illegal. The default 'dev' will be used.")
+	}
+}
