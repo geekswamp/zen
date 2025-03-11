1f2c498c0db51df8f73b731ea5f12afa1b22f3c9
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-11 20:47:43 +0700

    feat(errors): add new error for database connection and local timezone

--- pkg/errors/errs.go
+++ pkg/errors/errs.go
@@ -3,5 +3,7 @@ package errors
 import "errors"
 
 var (
-	ErrNotAFile = errors.New("Not a file")
+	ErrNotAFile          = errors.New("Not a file")
+	ErrFailedToConnectDB = errors.New("Failed to connect to database")
+	ErrFailedLoadLocal   = errors.New("Failed to load local timezone")
 )
