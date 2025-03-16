2b1d823afe702739091ec6555c64ff6ff9dffd26
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-16 15:39:23 +0700

    feat(errors): add new error for failed token parsing

--- pkg/errors/errs.go
+++ pkg/errors/errs.go
@@ -14,4 +14,5 @@ var (
 	ErrFailedToDecodeStr         = errors.New("failed to decode string")
 	ErrInvalidToken              = errors.New("invalid token")
 	ErrNilPubKey                 = errors.New("public key is nil")
+	ErrFailedTokenParsing        = errors.New("failed parsing token")
 )
