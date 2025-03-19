2ad0c9b0f9e380e74a9203c746ba24108799731a
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-19 15:35:17 +0700

    refactor(seeder): rename SeederManager to Manager

--- internal/storage/seeder/seeder.go
+++ internal/storage/seeder/seeder.go
@@ -11,23 +11,23 @@ var log = logger.New()
 // SeederFunc represents a function type that performs database seeding operations.
 type SeederFunc func(db *gorm.DB) error
 
-// SeederManager handles a collection of seed functions for populating database tables.
-type SeederManager struct {
+// Manager handles a collection of seed functions for populating database tables.
+type Manager struct {
 	seeders []SeederFunc
 }
 
-// New creates and returns a new instance of SeederManager.
-func New() *SeederManager {
-	return &SeederManager{}
+// New creates and returns a new instance of Manager.
+func New() *Manager {
+	return &Manager{}
 }
 
-// Add appends one or more seeder functions to the SeederManager's collection of seeders.
-func (s *SeederManager) Add(seeders ...SeederFunc) {
+// Add appends one or more seeder functions to the Manager's collection of seeders.
+func (s *Manager) Add(seeders ...SeederFunc) {
 	s.seeders = append(s.seeders, seeders...)
 }
 
 // Run executes all registered seeders sequentially using the provided database connection.
-func (s *SeederManager) Run(db *gorm.DB) error {
+func (s *Manager) Run(db *gorm.DB) error {
 	for _, seed := range s.seeders {
 		if err := seed(db); err != nil {
 			log.Fatal(errors.ErrFailedRunningSeeder.Error(), logger.ErrDetails(err))
