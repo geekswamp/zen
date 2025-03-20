adf4ca74244aa77b55b9721e3da069ed6a14de27
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-20 10:40:29 +0700

    refactor(model): rename timestamp fields for consistency

--- internal/base/model.go
+++ internal/base/model.go
@@ -3,16 +3,12 @@ package base
 import "github.com/google/uuid"
 
 // ID represents a custom type based on UUID for unique identification.
-// It provides type safety and distinct handling of UUID-based identifiers.
 type ID uuid.UUID
 
 // Model represents a base model structure with common fields used across entities.
-// It includes fields for unique identification, creation timestamp, last update timestamp,
-// and soft deletion timestamp. This struct is designed to be embedded in other models
-// to provide standard database record functionality.
 type Model struct {
-	ID        ID     `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CreatedAt int64  `json:"created_at" gorm:"autoCreateTime:milli"`
-	UpdatedAt *int64 `json:"updated_at" gorm:"autoUpdateTime:milli"`
-	DeletedAt *int64 `json:"deleted_at" gorm:"index"`
+	ID          ID     `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedTime int64  `json:"created_itme" gorm:"column:created_time;autoCreateTime:milli"`
+	UpdatedTime *int64 `json:"updated_itme" gorm:"column:updated_time;autoUpdateTime:milli"`
+	DeletedTime *int64 `json:"deleted_itme" gorm:"column:deleted_time;index"`
 }
