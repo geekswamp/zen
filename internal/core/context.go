b74a338d04f7eb83f22691bc2e0ca3e869a805e4
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-20 11:09:36 +0700

    docs(core): add docstring

--- internal/core/context.go
+++ internal/core/context.go
@@ -2,10 +2,13 @@ package core
 
 import "github.com/google/uuid"
 
+// Context holds request-scoped data for use throughout the application.
 type Context struct {
 	RequestID string
 }
 
+// GetRequestID generates and returns a new UUID as a string to uniquely identify a request.
+// This method ensures each request has a unique identifier for tracking and logging purposes.
 func (c *Context) GetRequestID() string {
 	c.RequestID = uuid.NewString()
 
