b55bdba1ebb225cc133a04ebbaa10256b6d13d87
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-21 08:49:11 +0700

    refactor(base): remove type ID

--- internal/base/model.go
+++ internal/base/model.go
@@ -2,13 +2,10 @@ package base
 
 import "github.com/google/uuid"
 
-// ID represents a custom type based on UUID for unique identification.
-type ID uuid.UUID
-
 // Model represents a base model structure with common fields used across entities.
 type Model struct {
-	ID          ID     `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CreatedTime int64  `json:"created_itme" gorm:"column:created_time;autoCreateTime:milli"`
-	UpdatedTime *int64 `json:"updated_itme" gorm:"column:updated_time;autoUpdateTime:milli"`
-	DeletedTime *int64 `json:"deleted_itme" gorm:"column:deleted_time;index"`
+	ID          uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedTime int64     `json:"created_time" gorm:"column:created_time;autoCreateTime:milli"`
+	UpdatedTime *int64    `json:"updated_time" gorm:"column:updated_time;autoUpdateTime:milli"`
+	DeletedTime *int64    `json:"deleted_time" gorm:"column:deleted_time;index"`
 }
