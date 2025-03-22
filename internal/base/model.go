16f26299dceeb3adbeba44993e26356a75b7a788
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-22 23:29:23 +0700

    refactor(base): remove pointer for UpdatedTime

--- internal/base/model.go
+++ internal/base/model.go
@@ -6,6 +6,6 @@ import "github.com/google/uuid"
 type Model struct {
 	ID          uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
 	CreatedTime int64     `json:"created_time" gorm:"column:created_time;autoCreateTime:milli"`
-	UpdatedTime *int64    `json:"updated_time" gorm:"column:updated_time;autoUpdateTime:milli"`
+	UpdatedTime int64     `json:"updated_time" gorm:"column:updated_time;autoUpdateTime:milli"`
 	DeletedTime *int64    `json:"deleted_time" gorm:"column:deleted_time;index"`
 }
