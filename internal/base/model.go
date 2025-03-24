5b8ceadea36611a4c41658dc29719d029cd2c5a8
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-24 10:09:03 +0700

    refactor(base): remove json tag

--- internal/base/model.go
+++ internal/base/model.go
@@ -4,8 +4,8 @@ import "github.com/google/uuid"
 
 // Model represents a base model structure with common fields used across entities.
 type Model struct {
-	ID          uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	CreatedTime int64     `json:"created_time" gorm:"column:created_time;autoCreateTime:milli"`
-	UpdatedTime int64     `json:"updated_time" gorm:"column:updated_time;autoUpdateTime:milli"`
-	DeletedTime *int64    `json:"deleted_time" gorm:"column:deleted_time;index"`
+	ID          uuid.UUID `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedTime int64     `gorm:"column:created_time;autoCreateTime:milli"`
+	UpdatedTime int64     `gorm:"column:updated_time;autoUpdateTime:milli"`
+	DeletedTime *int64    `gorm:"column:deleted_time;index"`
 }
