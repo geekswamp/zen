5b379e7eca7aa2a0a5216fcfeb0a5ec5d5c3bb7a
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-24 13:22:00 +0700

    feat(format): add snake case format

---
+++ cmd/genz/internal/format/format.go
@@ -0,0 +1,15 @@
+package format
+
+import "github.com/iancoleman/strcase"
+
+func ToCamelCase(text string) string {
+	return strcase.ToLowerCamel(text)
+}
+
+func ToPascalCase(text string) string {
+	return strcase.ToCamel(text)
+}
+
+func ToSnakeCase(text string) string {
+	return strcase.ToSnake(text)
+}
