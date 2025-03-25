35fb2a3d243b8af977f43b8c3596cc4841239348
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-25 10:29:37 +0700

    refactor(command): change method name from runE to runModelE

--- cmd/genz/internal/command/create/model.go
+++ cmd/genz/internal/command/create/model.go
@@ -5,21 +5,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var dir string
-
 var modelCmd = &cobra.Command{
 	Use:     "model <name> [-d dir]",
 	Short:   "Create a new model.",
 	Args:    cobra.MinimumNArgs(1),
 	Example: "genz create model user",
-	RunE:    runE,
+	RunE:    runModelE,
 }
 
 func init() {
 	modelCmd.Flags().StringVarP(&dir, "dir", "d", "", "Specify the model directory")
 }
 
-func runE(_ *cobra.Command, args []string) error {
+func runModelE(_ *cobra.Command, args []string) error {
 	m := new(template.Make)
 	m.FeatureName = args[0]
 	m.FileType = template.Model
