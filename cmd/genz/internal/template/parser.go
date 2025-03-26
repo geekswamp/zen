04c51526e4cee17b5eee5bde671b701223d430d2
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-26 10:41:14 +0700

    feat(template): add ParseModel for gorm model parser

--- cmd/genz/internal/template/parser.go
+++ cmd/genz/internal/template/parser.go
@@ -3,6 +3,7 @@ package template
 import (
 	"fmt"
 	"os"
+	"reflect"
 	"text/template"
 
 	"github.com/geekswamp/zen/cmd/genz/internal/format"
@@ -41,4 +42,36 @@ func (m Make) Parse(file *os.File) error {
 	}
 
 	return nil
+}
+
+func ParseModel(model any) (names []string, types []reflect.Type) {
+	t := reflect.TypeOf(model)
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+
+	if t.Kind() != reflect.Struct {
+		return nil, nil
+	}
+
+	var parseFields func(t reflect.Type)
+
+	parseFields = func(t reflect.Type) {
+		for i := range t.NumField() {
+			field := t.Field(i)
+
+			if field.Anonymous {
+				parseFields(field.Type)
+				continue
+			}
+
+			names = append(names, field.Name)
+			types = append(types, field.Type)
+		}
+	}
+
+	parseFields(t)
+
+	return names, types
 }
