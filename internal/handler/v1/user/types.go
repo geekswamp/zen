41c19f96e62503f424d57e62d1383ee996f5c510
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-03-29 13:28:12 +0700

    feat(user): add user request and response types

---
+++ internal/handler/v1/user/types.go
@@ -0,0 +1,31 @@
+package user
+
+import "github.com/google/uuid"
+
+type UserCreateRequest struct {
+	FullName string  `json:"full_name" validate:"required,min=3,max=100"`
+	Email    string  `json:"email" validate:"required,email"`
+	Phone    *string `json:"phone" validate:"e164"`
+	Gender   int     `json:"gender" validate:"required,number,gte=1"`
+	Password string  `json:"password" validate:"required,password"`
+}
+
+type UserUpdateInfoRequest struct {
+	FullName string  `json:"full_name" validate:"min=3,max=100"`
+	Email    string  `json:"email" validate:"email"`
+	Phone    *string `json:"phone" validate:"e164"`
+	Gender   int     `json:"gender" validate:"number,gte=1"`
+}
+
+type UserInfoResponse struct {
+	ID            uuid.UUID `json:"id"`
+	FullName      string    `json:"full_name"`
+	Email         string    `json:"email"`
+	Phone         *string   `json:"phone"`
+	Active        bool      `json:"active"`
+	Gender        int       `json:"gender"`
+	ActivatedTime int64     `json:"activated_time"`
+	CreatedTime   int64     `json:"created_time"`
+	UpdateTime    int64     `json:"updated_time"`
+	DeletedTime   *int64    `json:"deleted_time"`
+}
