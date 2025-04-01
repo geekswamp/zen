70b7522fd05811b9aa4604f84d39aeb14ec90e66
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-04-01 20:16:16 +0700

    docs(base): add dosctring

--- internal/base/repository.go
+++ internal/base/repository.go
@@ -2,14 +2,23 @@ package base
 
 import "gorm.io/gorm"
 
+// UpdateMap represents a map used for dynamic updates in repository operations.
+// The string key represents the field name to be updated, and the any value
+// represents the new value to be set for that field.
 type UpdateMap map[string]any
 
-type Repository struct{ db *gorm.DB }
+// Repository represents a base data access layer struct that holds a GORM database connection.
+// It serves as a foundation for specific repository implementations by providing access to the database instance.
+type Repository struct {
+	db *gorm.DB
+}
 
+// NewRepo creates a new instance of Repository with the provided gorm database connection.
 func NewRepo(db *gorm.DB) Repository {
 	return Repository{db: db}
 }
 
+// DB returns the GORM database instance used by the repository.
 func (r Repository) DB() *gorm.DB {
 	return r.db
 }
