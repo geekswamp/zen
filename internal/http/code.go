a16192be789711a64470d9226089a5c507f76d31
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-04-05 06:23:14 +0700

    faet(http): add error codes for user existence and resource not found

--- internal/http/code.go
+++ internal/http/code.go
@@ -6,6 +6,8 @@ var (
 	InputNotValid      = NewErrorCode("ERR-PA40002", "The provided input is not valid")
 	Forbidden          = NewErrorCode("ERR-PA40003", "Access to this resource is forbidden")
 	NotValidQuery      = NewErrorCode("ERR-PA40004", "The provided URL Query is not valid")
+	UserAlreadyExists  = NewErrorCode("ERR-PA40005", "User already exists. Please use a different email or phone number")
+	NotFound           = NewErrorCode("ERR-PA40006", "The requested resource was not found")
 	InvalidRequestID   = NewErrorCode("ERR-HR40001", "Invalid X-Request-ID format. It must be a valid UUID")
 	SystemError        = NewErrorCode("ERR-SY50001", "A system error has occurred, please try again later")
 )
