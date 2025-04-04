7d19061af2fed4fef22de68ae18a8d9591c5c285
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-04-04 14:43:46 +0700

    fix(repository): rename NewRepo to NewUserRepo for clarity

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -17,7 +17,7 @@ type UserRepository interface {
 
 type UserQueryBuilder struct{ repo base.Repository }
 
-func NewRepo(repo base.Repository) UserRepository {
+func NewUserRepo(repo base.Repository) UserRepository {
 	return UserQueryBuilder{repo: repo}
 }
 
