964d8e8c9a21463b2a353bb5ccefbe6f166218c9
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-04-05 10:01:20 +0700

    feat(main): initialize server and seeders in main function

--- cmd/zen/main.go
+++ cmd/zen/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/geekswamp/zen/configs"
+	"github.com/geekswamp/zen/internal/di"
+	"github.com/geekswamp/zen/internal/router"
+	"github.com/geekswamp/zen/internal/storage/seed"
+	"github.com/geekswamp/zen/pkg/http/middleware"
+	"github.com/geekswamp/zen/pkg/http/middleware/cors"
+	"github.com/geekswamp/zen/pkg/http/server"
+)
+
+func main() {
+	c := configs.Get()
+	s := server.New(
+		fmt.Sprintf("%s:%d", c.App.Host, c.App.Port),
+		server.SetMode(c.App.Mode),
+		server.Middlewares(cors.Default(), middleware.RequestID()),
+		server.ReadTimeout(30*time.Second),
+		server.WriteTimeout(30*time.Second),
+		server.RegisterRouter(router.RegisterRouter),
+	)
+
+	if err := seed.RunSeeders(di.ProvidePostgres()); err != nil {
+		panic(err)
+	}
+
+	s.Start()
+}
